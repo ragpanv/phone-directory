ded456c61ba914d4fad9f8bc1359486cf4b5abc4
ragpanv <raginianvekar@gmail.com>
2020-10-31 15:06:27 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,57 @@
-/*import React, {Component} from 'react';
-
-
-function App() {
-  let x=1;
-  let y=2;
-  return (
-    
-      <div className="header">
-           phone directory
-       
-       <button >Add </button>
-       <div>
-         <span>Name </span><br/>
-         <span>Phone </span>
-       </div>  
-       <label htmlFor="name">Name:</label>
-       <input id="name" defaultValue=x+y type="text" placeholder="Type here"/>
-       </div>
-     
-  );
-}
-*/
 import React, {Component} from 'react';
 import Header from "./Header.js";
 import './App.css';
 class App extends Component{
+
+  deleteHandler(message){
+    alert(message)
+  }
+  alterHandler(){
+    alert('alter the content')
+  }
   render(){
+    let subscribers=[
+      {
+          id: 1,
+          name: "abc",
+          phone : '12345'
+      },
+     {          id: 2,
+      name: 'abdeddc',
+      phone: '12345'
+    },
+     {   id: 3,
+      name: 'adsdbc',
+      phone: '12345'
+    }
+    ]
+  
 
     return(
-        <div>
-           
-            <Header />
+        <div className="component-container">
+          {/*comment */}
+            <Header heading="Phone directory"/>
             <div className="component-body-container">
             <button className="custom-btn add-btn">Add</button>
 
                 <div className="grid-container heading-container">
                     <span className="grid-item name-heading">Name</span>
-                    <span className="grid-item name-heading">Phone</span>
-                    <span className="grid-item phone-heading">Address</span>
+                    <span className="grid-item phne-heading">Phone</span>
                 </div> 
+
+            {
+              subscribers.map(sub =>{
+                return <div key={sub.id} className='grid-container'>
+                  <span className='grid-item'>{sub.name}</span>
+                  <span className='grid-item'>{sub.phone}</span>
+                  <span className="grit-item action-btn-container">
+                    <button className="custom-btn delete-btn" onClick={this.deleteHandler.bind(this,'Delete Clicked')}>Delete</button>
+                    <button className="custom-btn alter-btn" onClick={this.alterHandler}>Delete</button>
+
+                  </span>
+                  </div>
+              })
+            }
             </div>
         </div>
     );
