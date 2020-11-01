cda3e18b4468a3a41df42aa977b9b0777a19b174
ragpanv <72090309+ragpanv@users.noreply.github.com>
2020-11-02 01:18:38 +0530

    Update AddSubscriber.js

--- AddSubscriber.js
+++ AddSubscriber.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import Header from './Header'
-import './AddSubscriber.css'
+import Header from './Header';
+import './AddSubscriber.css';
+import {Link} from 'react-router-dom';
 
 class AddSubscriber extends Component{
 
@@ -20,14 +21,22 @@ class AddSubscriber extends Component{
         this.setState(state);
     }
 
+    onFormSubmitted=(e)=>{
+        e.preventDefault();
+        this.props.addSubscriberHandler(this.state);
+        this.setState({id: 0, name: '', phone:''});
+        this.props.history.push("/");
+    }
+
     render(){
         const {name, phone}=this.state;
         return (
             <div>
-                <Header heading="AddSubscriber "/>
+                <Header heading="Add Subscriber "/>
                 <div className="component-body-container">
-                    <button className='custom-btn'>Back</button>
-                    <form className='subscriber-form'>
+                    <Link to='/'><button className='custom-btn'>Back</button></Link>
+                   
+                    <form className="subscriber-form" onSubmit={this.onFormSubmitted.bind(this)}>
                         <label htmlFor="name" className='label-control'>Name: </label><br/>
                         <input id="name" type="text" className="input-control" name="name" onChange={this.inputChangedHandler}/><br/><br/>
                         <label htmlFor="phone" className='label-control'>Phone: </label><br/>
@@ -36,7 +45,7 @@ class AddSubscriber extends Component{
                         <div className="subscriber-info-container">
                             <span className="subscriber-to-add-heading">subscriber to be added: </span><br/>
                             <span className="subscriber-info">Name:{name} </span><br/>
-                            <span className="ubscriber-info">Phone:{phone} </span><br/>
+                            <span className="subscriber-info">Phone:{phone} </span><br/>
                         </div>
 
                         <button type="submit" className="custom-btn add-btn">Add</button>
